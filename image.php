5796f91b8e9aca4dc66e3b18a33f40741d69f727
Bala2703 <balakumar27032002@gmail.com>
2021-10-11 09:01:48 +0530

    title

--- image.php
+++ image.php
@@ -47,7 +47,7 @@ else {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Blogging</title>
 </head>
 <body> 
     <form action="uploads.php" method="POST" enctype="multipart/form-data">
--- listofusers.php
+++ listofusers.php
@@ -11,7 +11,7 @@ include "connection.php";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
-    <title>Document</title>
+    <title>Blogging</title>
 </head>
 <body>
 
